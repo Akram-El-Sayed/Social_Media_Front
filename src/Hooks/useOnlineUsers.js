243cda02c1872c47e3b8fd800073711708d8826f
Akram-El-Sayed <akram.adel2009@gmail.com>
2026-05-16 23:02:37 +0300

    Delete src directory

--- src/Hooks/useOnlineUsers.js
+++
@@ -1,5 +0,0 @@
-import { useContext } from "react";
-import OnlineContext from "../Context/onlineContextValue";
-
-/* Returns a Set<string> of currently-online user IDs. */
-export const useOnlineUsers = () => useContext(OnlineContext);
--- src/utils/TypeToEmoji/TypeToEmoji.js
+++
@@ -1,9 +0,0 @@
-export const REACTIONS = [
-  { emoji: "👍", type: "like"  },
-  { emoji: "❤️", type: "love"  },
-  { emoji: "😂", type: "haha"  },
-  { emoji: "😢", type: "sad"   },
-  { emoji: "😡", type: "angry" },
-];
-
-export const typeToEmoji = Object.fromEntries(REACTIONS.map((r) => [r.type, r.emoji]));
