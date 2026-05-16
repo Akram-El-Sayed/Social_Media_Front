34faec9e6e832186153c24ef33b98d91b38a011b
Akram-El-Sayed <akram.adel2009@gmail.com>
2026-05-16 23:13:03 +0300

    Delete src directory

--- src/utils/api.jsx
+++
@@ -1,7 +0,0 @@
-// e.g. utils/api.js
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:5000",
-  withCredentials: true,
-});
