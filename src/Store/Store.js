b7ec4f3626a465e5820907ccba8a2c28193d4aa0
Akram-El-Sayed <akram.adel2009@gmail.com>
2026-05-17 15:45:37 +0300

    Add reelsReducer to the store configuration

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./UserSlice/UserSlice"
 import notificationReducer from './NotificationSlice/NotificationSlice'
 import feedReducer from './feedSlice/feedSlice'
+import reelsReducer from "./reelsSlice/reelsSlice";
 
 
 // Store States Global
@@ -11,10 +12,11 @@ export const Store = configureStore({
   reducer: {
     // user -> useSelector(state => state.user)
    user: userReducer,
-    // cart -> useSelector(state => state.cart)
     // useSelector(state => state.notification)
     notification: notificationReducer,
 
     feed: feedReducer,
+
+    reels: reelsReducer,
   },
 });
